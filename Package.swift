ac7f2b2483780444014a6038ac4764abd082681c
Ferran Poveda <fbeeper@me.com>
2026-05-15 16:15:59 +0200

    fixing some legit loose ends unearthed by the swift package index builds

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [
         .macOS(.v15),
-        .iOS(.v17),
-        .tvOS(.v17),
-        .watchOS(.v10),
-        .visionOS(.v1),
-        .macCatalyst(.v17),
+        .iOS(.v18),
+        .tvOS(.v18),
+        .watchOS(.v11),
+        .visionOS(.v2),
+        .macCatalyst(.v18),
     ],
     products: [
         .library(name: "AgentKitten", targets: [
